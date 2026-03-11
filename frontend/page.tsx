38442d0083dc9fc8f50a1fec302f854d35f98669
Shreyasoni20 <shreyasoni172@gmail.com>
2026-03-11 23:02:50 +0530

    Create page.tsx

---
+++ frontend/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+export default function Home(){
+
+return(
+
+<div style={{padding:40}}>
+
+<h1>Analytica Dashboard</h1>
+
+<Link href="/ai-dashboard-builder">
+Go to AI Dashboard Builder
+</Link>
+
+</div>
+
+)
+
+}
